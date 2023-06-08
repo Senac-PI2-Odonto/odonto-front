876c2f087099c6babb92e360edfdc56f538f4905
Dayvson Lima <dayvsonlsantos@gmail.com>
2023-06-08 11:10:26 -0300

    fix: Corrigindo Regex

---
+++ _js/validarTextArea.js
@@ -0,0 +1,13 @@
+function validateTextarea(event) {
+    event.preventDefault();
+    var textarea = document.getElementById("mensagem");
+    // var pattern = new RegExp("^[A-Za-zÀ-ÿ'.,;:!?@ -]{10,50}$");
+
+    var regex = /^[A-Za-zÀ-ÿ\s\'\-\,\.\;\:\!\?\@\0-9\º\ª]{10,500}$/;
+
+    if (regex.test(textarea.value)) {
+      textarea.setCustomValidity("");
+    } else {
+      textarea.setCustomValidity("O motivo deve ter pelo menos 10 caracteres.");
+    }
+  }
